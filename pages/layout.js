6f337c7aff07697f5197927b669aeb907f8af721
Philip Kisaih Iringo <78140802+Luqman-tech@users.noreply.github.com>
2024-12-14 16:26:19 +0300

    Update layout.js

--- pages/layout.js
+++ pages/layout.js
@@ -1,8 +1,7 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const inter = Inter({
   subsets: ["latin"],
 });
 
