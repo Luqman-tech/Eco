d7658a48881ccea601708582b7f32ae257bbdeac
Philip Kisaih Iringo <78140802+Luqman-tech@users.noreply.github.com>
2024-12-15 22:46:36 +0300

    Update projects.js

--- pages/projects.js
+++ pages/projects.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Head from 'next/head'
 import Script from 'next/script'
 
-export default function Projects() {
+const Projects = () => {
     return (
         <>
         <Head>
