38b3eeeac6728f281bff45ef26938f59efb7d383
Luqman-tech <rexalchem69@gmail.com>
2024-12-15 14:48:14 +0300

    update

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,5 @@
-<<<<<<< HEAD
-import '../styles/globals.css'
-
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
 
-=======
-import '../styles/globals.css'
-
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
-
->>>>>>> 19478cb5094be8d975b0510fb969f515eca07760
 export default MyApp
