26d744bbf0a850026074f3aa7bed284cd8d345c8
Philip Kisaih Iringo <78140802+Luqman-tech@users.noreply.github.com>
2024-12-15 18:18:41 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,14 +1,16 @@
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   output: 'standalone',
   
-  // CSS Modules support
-  webpack: (config) => {
+  // CSS Configuration
+  webpack: (config, { isServer }) => {
     config.module.rules.push({
       test: /\.css$/,
       use: ['style-loader', 'css-loader']
     });
+
     return config;
   }
 };
