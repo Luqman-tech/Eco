71a755febd439ee202b94fb4ffae437a9e3a0092
Philip Kisaih Iringo <78140802+Luqman-tech@users.noreply.github.com>
2024-12-15 14:58:26 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'standalone'
+  output: 'standalone',
+  
+  // Optional: Webpack configuration
+  webpack: (config) => {
+    config.resolve.fallback = { 
+      fs: false,
+      net: false,
+      tls: false 
+    };
+    return config;
+  }
 };
 
 module.exports = nextConfig;
