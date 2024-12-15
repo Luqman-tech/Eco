36a0c23608960813252b6200a84c0d8bdc679c46
Philip Kisaih Iringo <78140802+Luqman-tech@users.noreply.github.com>
2024-12-15 15:17:50 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -2,8 +2,6 @@ function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
 
-<<<<<<< HEAD
 export default MyApp
-=======
-export default MyApp
->>>>>>> cd54afdedb9330962fce3523d1a485b3f558cff0
+
+
