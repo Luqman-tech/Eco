e9cd0ad6f2d29459af5d08e2f889ab162506e09c
Philip Kisaih Iringo <78140802+Luqman-tech@users.noreply.github.com>
2024-12-15 22:22:08 +0300

    Update about-us.js

--- pages/about-us.js
+++ pages/about-us.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Head from 'next/head'
 import Script from 'next/script'
 
-function Home() {
+export default function About() {
     return (
         <>
         <Head>
