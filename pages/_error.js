1cffff4d779e998eadc5db6925a6d0fe9544c029
Philip Kisaih Iringo <78140802+Luqman-tech@users.noreply.github.com>
2024-12-15 16:45:20 +0300

    Update _error.js

--- pages/_error.js
+++ pages/_error.js
@@ -1,18 +1,19 @@
 function Error({ statusCode }) {
-    return (
-      <div>
-        <h1>
-          {statusCode
-            ? `An error ${statusCode} occurred on server`
-            : 'An error occurred on client'}
-        </h1>
-      </div>
-    );
-  }
-  
-  Error.getInitialProps = ({ res, err }) => {
-    const statusCode = res ? res.statusCode : err ? err.statusCode : 404;
-    return { statusCode };
-  };
-  
-  export default Error;
+  return (
+    <div>
+      <h1>
+        {statusCode
+          ? `An error ${statusCode} occurred on server`
+          : 'An error occurred on client'}
+      </h1>
+      <p>We apologize for the inconvenience. Please try again later.</p>
+    </div>
+  );
+}
+
+Error.getInitialProps = ({ res, err }) => {
+  const statusCode = res ? res.statusCode : err ? err.statusCode : 404;
+  return { statusCode };
+};
+
+export default Error;
